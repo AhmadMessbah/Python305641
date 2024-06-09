107b4bcca8207d9626f58f5900aafa1480b0e0c0
saeedd5 <saeedd5@yahoo.com>
2024-06-09 08:32:21 +0330

    omidi /create job_test & update job

--- mftplus/model/tools/validator.py
+++ mftplus/model/tools/validator.py
@@ -8,3 +8,9 @@ def military_serial_validator(serial):
 
 def bank_validator(bank):
     return re.match("^[A-Za-z]{10}$", bank)
+
+def title_validator(title):
+    if re.match("^[A-Za-z]{10}$", title):
+        return title
+    else:
+        return "Invalid title"
