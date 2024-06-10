f570966b005427defe173fda058aba5c2294bd28
mohammad hessam sarbishe <unitdesign011@gmail.com>
2024-06-10 20:05:13 +0330

    mohammad hessam sarbisheh military-class

--- mftplus/model/entity/person.py
+++ mftplus/model/entity/person.py
@@ -3,17 +3,27 @@ from sqlalchemy import Column, Integer, String, Boolean
 from mftplus.model.entity.base import Base
 
 
-class Person(Base):
-    __tablename__ = "person_tbl"
-    _id = Column("id", Integer, primary_key=True, autoincrement=True)
-    _name = Column("name", String(20), nullable=False)
-    _family = Column("family", String(20), nullable=False)
-    _status = Column("status", Boolean, default=True)
+class Military(Base):
+    __tablename__ = "military_tbl"
 
-    def __init__(self, name, family, status=True):
-        self.id = None
-        self.name = name
-        self.family = family
-        self.status = status
+
+    _id = Column(Integer, primary_key=True , autoincrement=True)
+    _serial = Column("serial", Integer(30))
+    _date = Column("date", Integer(20))
+    _location = Column("location", Integer(20))
+    _organization = Column("organization", Integer(30))
+    _status = Column("status", Integer(20))
+    _deleted = Column("deleted", default=False)
+
+    def __init__(self,serial , date , location , organization , status , deleted):
+        self._id = None
+        self._serial = serial
+        self._date = date
+        self._location = location
+        self._organization = organization
+        self._status  = status
+        self._deleted = deleted
 
     # getter / setter
+
+    #serial  date  location organization status deleted
