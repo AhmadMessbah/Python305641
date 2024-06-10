82f0c0024159fe7281b656764175943517d3b96f
mohammad hessam sarbishe <unitdesign011@gmail.com>
2024-06-10 20:09:59 +0330

    mohammad hessam sarbisheh military-class

--- mftplus/model/entity/person.py
+++ mftplus/model/entity/person.py
@@ -5,17 +5,15 @@ from mftplus.model.entity.base import Base
 
 class Military(Base):
     __tablename__ = "military_tbl"
-
-
     _id = Column(Integer, primary_key=True , autoincrement=True)
     _serial = Column("serial", Integer(30))
     _date = Column("date", Integer(20))
     _location = Column("location", Integer(20))
     _organization = Column("organization", Integer(30))
     _status = Column("status", Integer(20))
-    _deleted = Column("deleted", default=False)
+    _deleted = Column("deleted",Boolean, default=False)
 
-    def __init__(self,serial , date , location , organization , status , deleted):
+    def __init__(self,id ,serial , date , location , organization , status , deleted):
         self._id = None
         self._serial = serial
         self._date = date
