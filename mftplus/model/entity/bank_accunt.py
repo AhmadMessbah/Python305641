6931afe389e0ea02376a30fc83a2106d6d85eb31
Arshida <adrina.gashtasbi@gmail.com>
2024-06-10 20:06:40 +0330

    Bank Accunt Entity Add.

---
+++ mftplus/model/entity/bank_accunt.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from mftplus.model.entity.base import Base
+
+class BankAccount(Base):
+    __tablename__ = "bank_account"
+    _id = Column("id",Integer, primary_key=True, autoincrement=True)
+    _bank = Column("bank",String(30), nullable=False)
+    _branch = Column("branch",String(30), nullable=False)
+    _account_number = Column("account_number",Integer(10), nullable=False)
+    _card_number = Column("card_number",Integer(16), nullable=False)
+    _account_type = Column("account_type",String(20), nullable=False)
+    _status = Column("status",Boolean, default=True)
