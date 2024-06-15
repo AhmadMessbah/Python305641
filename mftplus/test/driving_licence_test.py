f577a958db9fac331d474e682bcefd45c553a7ef
bashirch <bmw5073477@gmail.com>
2024-06-15 20:14:01 +0330

    Bashir Update

--- mftplus/test/driving_licence_test.py
+++ mftplus/test/driving_licence_test.py
@@ -4,5 +4,11 @@ from mftplus.model.entity.person import Person
 
 
 drive_lice = DrivingLicense("16541651", "b1", "1/2/2020", "1/2/2030")
-drive_lice_da = DataAccess("driving_licence_da")
+#drive_lice_da = DataAccess(DrivingLicense)
+#drive_lice_da.save(drive_lice)
 print(drive_lice)
+
+#person = Person("bashir", "charkhab")
+#person.da = DataAccess("person")
+#person.da.save(person)
+#print(person)
