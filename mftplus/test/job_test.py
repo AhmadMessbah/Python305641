f2365b6896627d96a29aa30f85bee13a3f77bb69
Messbah.A <Messbah.A@Gmail.com>
2024-06-15 20:34:47 +0330

    messbah updated

--- mftplus/test/job_test.py
+++ mftplus/test/job_test.py
@@ -2,14 +2,14 @@ from mftplus.model.da.da import DataAccess
 from mftplus.model.entity.job import Job
 from mftplus.model.entity.person import Person
 
-person = Person("ali","omidy")
-person_da = DataAccess(Person)
-person_da.save(person)
-print(person)
+# person = Person("ali","omidy")
+# person_da = DataAccess(Person)
+# person_da.save(person)
+# print(person)
 
 
 job = Job("programming","google","2010/10/10","2020/10/20",True)
-job.person = person
+# job.person = person
 job_da = DataAccess(job)
 job_da.save(job)
 print(job)
