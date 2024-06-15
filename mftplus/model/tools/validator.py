924cb85f02005f8a13612815c12d18998e9c9ce5
kiyana shirzadi (car) <kiyanashirzadi2003@gmail.com>
2024-06-15 19:47:42 +0330

    kiyana shirzadi car

--- mftplus/model/tools/validator.py
+++ mftplus/model/tools/validator.py
@@ -1,19 +1,19 @@
 import re
 
 def car_id_validator(id):
-    return re.match("^[\d]{2,30}$", id)
+    return re.match("r^[\d]{2,30}$", id)
 
 
 def car_name_validator(name):
-    return isinstance(name, str) and bool(re.match("^[a-zA-Z\s]{2,30}$", name))
+    return isinstance(name, str) and bool(re.match(r"^[a-zA-Z\s]{2,30}$", name))
 
 
 def car_model_validator(model):
-    return isinstance(model, str) and bool(re.match("^[a-zA-Z\d\s]{2,30}$", model))
+    return isinstance(model, str) and bool(re.match(r"^[a-zA-Z\d\s]{2,30}$", model))
 
 
 def car_man_date_validator(man_date):
-    return isinstance(man_date, int) and re.match("^\d{4}/\d{2}/\d{2}$", man_date)
+    return isinstance(man_date, int) and re.match(r"^\d{4}/\d{2}/\d{2}$", man_date)
 
 
 
