7ce749875ff56a8ce9fd2bcc04f61b43800f18e4
Arshida <adrina.gashtasbi@gmail.com>
2024-06-15 17:36:10 +0330

    bank accunt entity add.

--- mftplus/model/entity/bank_accunt.py
+++ mftplus/model/entity/bank_accunt.py
@@ -11,3 +11,12 @@ class BankAccount(Base):
     _card_number = Column("card_number",Integer(16), nullable=False)
     _account_type = Column("account_type",String(20), nullable=False)
     _status = Column("status",Boolean, default=True)
+
+    def __init__(self,id,bank,branch,accunt_number,card_number,accunt_type,status):
+        self._id = None
+        self._bank = bank
+        self._branch = branch
+        self._account_number = accunt_number
+        self._card_number = card_number
+        self._account_type = accunt_type
+        self._status = status
