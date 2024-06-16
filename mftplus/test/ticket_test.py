adbd67113504cdc6024c06b60347c470f1eb3fa9
Farbod <farbodowrang98@gmail.com>
2024-06-17 02:14:57 +0330

    Farbod orang

--- mftplus/test/ticket_test.py
+++ mftplus/test/ticket_test.py
@@ -1,8 +1,16 @@
 from mftplus.model.da.da import DataAccess
 from mftplus.model.entity.ticket import Ticket
+from mftplus.model.entity.person import Person
 
-tick1 = Ticket("group", "title", "description", "sender", "2024-06-15")
+from datetime import datetime
+
+now = datetime.today()
+pers1 = Person("amir", "amiri")
+da = DataAccess(Person)
+da.save(pers1)
+
+tick1 = Ticket(1, "group", "title", "description", "sender", date_time=now)
+print(tick1)
 
 ticket_da = DataAccess(Ticket)
-ticket_da.save(Ticket)
-print(tick1)
+ticket_da.save(tick1)
