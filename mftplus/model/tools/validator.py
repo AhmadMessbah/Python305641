21aa1f09df4e93798140ad337781a27a3fe93520
saeedd5 <saeedd5@yahoo.com>
2024-06-18 07:58:04 +0330

    omidi /update validator

--- mftplus/model/tools/validator.py
+++ mftplus/model/tools/validator.py
@@ -13,6 +13,12 @@ def military_serial_validator(serial):
 def bank_validator(bank):
     return re.match("^[A-Za-z]{10}$", bank)
 
+def title_validator(title):
+    if isinstance(title, str) and re.match(r"^[A-Za-z]{10}$", title):
+        return title
+    raise ValueError("Invalid title")
+
+
 
 class TicketValidator:
     @classmethod
