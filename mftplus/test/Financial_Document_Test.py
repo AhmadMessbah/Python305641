36539364191372c39945ffdbdec70140b97c2bdf
SiminEghbali <simin.eghbali@gmail.com>
2024-06-21 19:36:19 +0330

    Simin

--- mftplus/test/Financial_Document_Test.py
+++ mftplus/test/Financial_Document_Test.py
@@ -1,3 +1,10 @@
+from mftplus.model.da.da import DataAccess
+from mftplus.model.entity.Financial_Document import FinancialDocument
+from mftplus.model.entity.person import Person
+
+Financial_Document = FinancialDocument("2222","55555","2024/01/01","recive","bbb","True")
+Financial_Document_da = FinancialDocument(Financial_Document)
+Financial_Document_da.save(FinancialDocument)
 
 person = Person("Simin","Eghbali")
 person.bank_account = bank_account
