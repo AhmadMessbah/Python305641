5f61103de93c2f399c486d94d15957aec33de65a
Messbah <Messbah.A@Gmail.com>
2024-06-24 19:55:31 +0330

    letter service todo

--- mftplus/model/service/letter_service.py
+++ mftplus/model/service/letter_service.py
@@ -2,9 +2,12 @@ from mftplus.controller.exceptions.exeptions import LetterNotFoundError
 from mftplus.model.da.da import DataAccess
 from mftplus.model.entity.letter import Letter
 
+
 class LetterService:
     @staticmethod
     def save(letter):
-        letter_da=DataAccess(letter)
+        letter_da = DataAccess(letter)
         letter_da.save(letter)
 
+
+# todo: write find_by sender ,receiver, letter_group, title, priority
