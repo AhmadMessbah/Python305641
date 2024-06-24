edf94d5739e605911bae1fa7b1487f51dcfe5f5a
Messbah <Messbah.A@Gmail.com>
2024-06-24 20:19:37 +0330

    ticket service todo

--- mftplus/controller/exceptions/exeptions.py
+++ mftplus/controller/exceptions/exeptions.py
@@ -5,8 +5,12 @@ class PersonNotFoundError(Exception):
 
 class LetterNotFoundError(Exception):
     def __init__(self, *args):
-        super().__init__("letter Not Found !!!")
+        super().__init__("Letter Not Found !!!")
 
 class LessonNotFoundError(Exception):
     def __init__(self,*args):
-        super().__init__("lesson Not Found !!!")
+        super().__init__("Lesson Not Found !!!")
+
+class TicketNotFoundError(Exception):
+    def __init__(self,*args):
+        super().__init__("Ticket Not Found !!!")
