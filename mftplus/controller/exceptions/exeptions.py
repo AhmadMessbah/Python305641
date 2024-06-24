66b90b676c557698988226355d47b991b9f08d36
Messbah <Messbah.A@Gmail.com>
2024-06-24 20:32:20 +0330

    medical report service todo

--- mftplus/controller/exceptions/exeptions.py
+++ mftplus/controller/exceptions/exeptions.py
@@ -7,14 +7,22 @@ class JobNotFoundError(Exception):
     def __init__(self, *args):
         super().__init__("Job Not Found !!!")
 
+
 class LetterNotFoundError(Exception):
     def __init__(self, *args):
         super().__init__("Letter Not Found !!!")
 
+
 class LessonNotFoundError(Exception):
-    def __init__(self,*args):
+    def __init__(self, *args):
         super().__init__("Lesson Not Found !!!")
 
+
 class TicketNotFoundError(Exception):
-    def __init__(self,*args):
+    def __init__(self, *args):
         super().__init__("Ticket Not Found !!!")
+
+
+class CarNotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Car Not Found !!!")
