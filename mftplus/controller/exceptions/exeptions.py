9e3ee1f7088c7448401559354b73843fb565aa4a
saeedd5 <saeedd5@yahoo.com>
2024-06-25 08:25:26 +0330

    omidi / create job_service

--- mftplus/controller/exceptions/exeptions.py
+++ mftplus/controller/exceptions/exeptions.py
@@ -3,6 +3,11 @@ class PersonNotFoundError(Exception):
         super().__init__("Person Not Found !!!")
 
 
+class JobNotFoundError(Exception):
+    def __init__(self, *args):
+        super().__init__("Job Not Found !!!")
+
+
 class LetterNotFoundError(Exception):
     def __init__(self, *args):
         super().__init__("letter Not Found !!!")
