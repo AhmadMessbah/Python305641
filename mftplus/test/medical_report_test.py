93e5ea980d09992d87043309f0f8dd27c6f57e39
Roya-Mirhosseini <royamirhoseini95@gmail.com>
2024-06-29 03:24:42 +0330

    Roya Mirhosseini Medical Report is committed

--- mftplus/test/medical_report_test.py
+++ mftplus/test/medical_report_test.py
@@ -2,12 +2,12 @@ from mftplus.model.da.da import DataAccess
 from mftplus.model.entity.person import Person
 from mftplus.model.entity.medical_report import MedicalReport
 
-medical_report = MedicalReport("stroke","2", None)
+medical_report = MedicalReport("stroke","neorology", None)
 medical_report_da = DataAccess(MedicalReport)
 medical_report_da.save(medical_report)
 print(medical_report)
 
-person = Person("ahmad","messbah")
+person = Person("hamid","hamidi")
 person.medical_report = medical_report
 person_da = DataAccess(Person)
 person_da.save(person)
