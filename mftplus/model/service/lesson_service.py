ff6949d81528028529ae64983788a2d8fe7a07b5
Maedeh <Maedeh.Gholami24@gmail.com>
2024-06-29 14:04:03 +0330

    maedeh updated

--- mftplus/model/service/lesson_service.py
+++ mftplus/model/service/lesson_service.py
@@ -37,3 +37,20 @@ class LessonService:
         return lesson_da.find_by_id(id)
 
     # todo : group, department, title, code
+
+    @staticmethod
+    def find_by_lesson_group(lesson_group):
+        lesson_da = DataAccess(Lesson)
+        return lesson_da.find_by_lesson_group(lesson_group)
+    @staticmethod
+    def find_by_department(department):
+        lesson_da = DataAccess(Lesson)
+        return lesson_da.find_by_department
+
+    @staticmethod
+    def find_by_title(title):
+        lesson_da = DataAccess(Lesson)
+        return lesson_da.find_by(Lesson.title == title)
+    def find_by_code(code):
+        lesson_da = DataAccess(Lesson)
+        return lesson_da.find_by_code
