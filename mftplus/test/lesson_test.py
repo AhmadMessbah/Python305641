0c790e99594476891fdca63ba0ba950d64a1c487
maedehgholamii <maedeh.gholami24@gmail.com>
2024-06-29 20:17:17 +0330

    Maedeh updated

--- mftplus/test/lesson_test.py
+++ mftplus/test/lesson_test.py
@@ -1,9 +1,8 @@
 from mftplus.model.da.da import DataAccess
 from mftplus.model.entity.lesson import Lesson
 from mftplus.model.entity.person import Person
+from mftplus.controller.lesson_controller import LessonController
 
-lesson = Lesson("a","english","vocabulary","23","messbah","True","False")
-Person.lesson = lesson
-lesson_da = DataAccess(Lesson)
-lesson_da.save(lesson)
-print(lesson)
+print(LessonController.save("a", "english","vocab","12","messbah",))
+
+
