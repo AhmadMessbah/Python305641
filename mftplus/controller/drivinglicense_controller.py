b59f16f5cd0672e09a0576ecc40941044e37e5bd
bashirch <bmw5073477@gmail.com>
2024-06-29 17:35:44 +0330

    charkhab

---
+++ mftplus/controller/drivinglicense_controller.py
@@ -0,0 +1,86 @@
+from mftplus.model.service.drive_license_service import DriveLicenseService
+from mftplus.model.tools.logger import Logger
+
+
+class DrivingLicenseController:
+    @staticmethod
+    def save(drive_license):
+        try:
+            DriveLicenseService.save(drive_license)
+            Logger.info(f"Driving License Saved - {drive_license}")
+            return True, drive_license
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def edit(drive_license):
+        try:
+            if DriveLicenseService.find_by_id(drive_license.id):
+                DriveLicenseService.edit(drive_license)
+                Logger.info(f"drive license Edited - {drive_license}")
+                return drive_license
+            else:
+                pass
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def remove(id):
+        try:
+            drive_license = DriveLicenseService.remove(id):
+            Logger.info(f"drive license Removed - {drive_license}")
+            return True , drive_license
+
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_all():
+        try:
+            return DriveLicenseService.find_all()
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_by_id(id):
+        try:
+            return DriveLicenseService.find_by_id(id)
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_by_start_date_time(start_date_time):
+        try:
+            return DriveLicenseService.find_by_start_date_time(start_date_time)
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_by_expire_date_time(expire_date_time):
+        try:
+            return DriveLicenseService.find_by_expire_date_time(expire_date_time)
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_by_license_type(license_type):
+        try:
+            return DriveLicenseService.find_by_license_type(license_type)
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
+
+    @staticmethod
+    def find_by_serial(serial):
+        try:
+            return DriveLicenseService.find_by_serial(serial)
+        except Exception as e:
+            Logger.error(f"{e}")
+            return False, f"{e}"
---
+++ mftplus/model/service/drive_license_service.py
@@ -0,0 +1,58 @@
+from mftplus.model.da.da import DataAccess
+from mftplus.model.entity.driving_licence import DrivingLicense
+
+
+class DriveLicenseService:
+    @staticmethod
+    def save(drive_license):
+        drive_lice_da = DataAccess(DrivingLicense)
+        drive_lice_da.save(drive_license)
+
+    @staticmethod
+    def edit(drive_license):
+        drive_lice_da = DataAccess(DrivingLicense)
+        if drive_lice_da.find_by_id(drive_license.id):
+            drive_lice_da.edit(drive_license)
+            return drive_license
+        else:
+            pass
+
+    @staticmethod
+    def remove(id):
+        drive_lice_da = DataAccess(DrivingLicense)
+        if drive_lice_da.find_by_id(id):
+            return drive_lice_da.remove(id)
+        else:
+            pass
+
+    @staticmethod
+    def find_all():
+        drive_lice_da = DataAccess(DrivingLicense)
+        return drive_lice_da.find_all()
+
+    @staticmethod
+    def find_by_id(id):
+        drive_lice_da = DataAccess(DrivingLicense)
+        return drive_lice_da.find_by_id(id)
+
+    @staticmethod
+    def find_by_start_date_time(start_date_time):
+        drive_lice_da = DataAccess(DrivingLicense)
+        return drive_lice_da.find_by_start_date_time(start_date_time)
+
+    @staticmethod
+    def find_by_expire_date_time(expire_date_time):
+        drive_lice_da = DataAccess(DrivingLicense)
+        return drive_lice_da.find_by_expire_date_time(expire_date_time)
+
+    @staticmethod
+    def find_by_license_type(license_type):
+        drive_lice_da = DataAccess(DrivingLicense)
+        return drive_lice_da.find_by_license_type(license_type)
+
+    @staticmethod
+    def find_by_serial(serial):
+        drive_lice_da = DataAccess(DrivingLicense)
+        return drive_lice_da.find_by_serial(serial)
+
+    # todo : find by start_date_time,expire_date_time,license_type,serial
