fe2d243c1e2187d52a5106a814568faaa561b623
Roya-Mirhosseini <royamirhoseini95@gmail.com>
2024-07-06 01:59:28 +0330

    medical report

--- mftplus/model/service/medical_report_service.py
+++ mftplus/model/service/medical_report_service.py
@@ -1,17 +1,18 @@
 from mftplus.controller.exceptions.exeptions import MedicalReportNotFoundError
 from mftplus.model.da.da import DataAccess
 from mftplus.model.entity.medical_report import MedicalReport
+from mftplus.model.tools.decorators import *
 
 
 class MedicalReportService:
-    @staticmethod
-    def save(medical_report):
+    @classmethod
+    def save(cls,medical_report):
         medical_report_da = DataAccess(MedicalReport)
         medical_report_da.save(medical_report)
         return medical_report
 
-    @staticmethod
-    def edit(medical_report):
+    @classmethod
+    def edit(cls,medical_report):
         medical_report_da = DataAccess(MedicalReport)
         if medical_report_da.find_by_id(medical_report.id):
             medical_report_da.edit(medical_report)
@@ -19,26 +20,26 @@ class MedicalReportService:
         else:
             raise MedicalReportNotFoundError()
 
-    @staticmethod
-    def remove(id):
+    @classmethod
+    def remove(cls,id):
         medical_report_da = DataAccess(MedicalReport)
         if medical_report_da.find_by_id(id):
             return medical_report_da.remove(id)
         else:
             raise MedicalReportNotFoundError()
 
-    @staticmethod
-    def find_all():
+    @classmethod
+    def find_all(cls,):
         medical_report_da = DataAccess(MedicalReport)
         return medical_report_da.find_all()
 
-    @staticmethod
-    def find_by_id(id):
+    @classmethod
+    def find_by_id(cls,id):
         medical_report_da = DataAccess(MedicalReport)
         return medical_report_da.find_by_id(id)
 
-    @staticmethod
-    def find_by_disease(disease):
+    @classmethod
+    def find_by_disease(cls,disease):
         medical_report_da = DataAccess(MedicalReport)
         return medical_report_da.find_by(MedicalReport.disease == disease)
 
--- mftplus/model/tools/validator.py
+++ mftplus/model/tools/validator.py
@@ -191,7 +191,8 @@ def code_validator(code, message):
 
 
 # medical_report
-def disease_validator(disease, message):
+@classmethod
+def disease_validator(cls,disease, message):
     if isinstance(disease, str) and re.match(r"^[a-zA-Z\s]{3,30}$", disease):
         return disease
     else:
